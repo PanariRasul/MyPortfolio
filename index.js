a52ca577ba48b12b976f073b908815b95921d00c
PanariRasul <panarirasul@gmail.com>
2023-10-07 23:50:27 +0530

    js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const fileSystem = require("fs");
-fileSystem.writeFile("noddy.txt","Hello Noddy!!!!", function(err, result) {
-    if(err) {
-        console.log("Error",err);
-    };
-});
+// const fileSystem = require("fs");
+// fileSystem.writeFile("noddy.txt","Hello Noddy!!!!", function(err, result) {
+//     if(err) {
+//         console.log("Error",err);
+//     };
+// });
+
+// const fileSystem = require("fs");
+// fileSystem.mkdir("bsdk", (err,result) =>{
+//     if(err){
+//         console.log(err,"error is occured Re check your code");
+//     }
+// })
+// const http = require ("http");
+// http.createServer((request, response) => {
+//     console.log(request.headers)
+//     response.end("Hello It From BackEnd")
+// }).listen(8000);
 
 
+const fileSystem = require ("fs");
+fileSystem.rmfile("noddy.txt", (err,result) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Directory removed sucsessfully!!!");
+    }
+})
