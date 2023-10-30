99f33c2f762394109b2a2a92426d20c06fd28b15
PanariRasul <panarirasul@gmail.com>
2023-10-30 17:08:45 +0530

    index.js

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-// const fileSystem = require("fs");
-// fileSystem.writeFile("noddy.txt","Hello Noddy!!!!", function(err, result) {
-//     if(err) {
-//         console.log("Error",err);
-//     };
-// });
+const soundCloud = document.querySelector('.sound-cloud');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
 
-// const fileSystem = require("fs");
-// fileSystem.mkdir("bsdk", (err,result) =>{
-//     if(err){
-//         console.log(err,"error is occured Re check your code");
-//     }
-// })
-// const http = require ("http");
-// http.createServer((request, response) => {
-//     console.log(request.headers)
-//     response.end("Hello It From BackEnd")
-// }).listen(8000);
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
 
-
-const fileSystem = require ("fs");
-fileSystem.rmdir("bsdk", (err,result) => {
-    if(err){
-        console.log(err);
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        soundCloud.style.color = "#08fdd8";
+        myAudio.play();
     }
-    else{
-        console.log("Directory removed sucsessfully!!!");
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        soundCloud.style.color = "#f50057";
+        myAudio.pause();
     }
-})
+}
+
+// Play music functionality
+
+const btnBars = document.querySelector('.bars');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+}
